e8741483d174656d6ad659fa88a05ab2c40ba3f8
HugoJimenzz <thehugo031@gmil.com>
2022-03-10 09:12:38 +0100

    V3

--- index.java
+++ index.java
@@ -1,10 +1,10 @@
 public static boolean esPrimo(int n)
 {
     int i=2;
-    while(n<1){
+    while(n<5){
         return false;
     }
-    while (i<n){
+    while (n<i){
         if(n%i==0) {
             return false;
         }    
